aaf7bc2dcb24fddf05e7d5b16f6f3a12fbba1ec6
haiyn <corexaren@gmail.com>
2021-03-29 21:14:52 +0200

    updated dag

--- src/airflow/dags/mtg-fetch.py
+++ src/airflow/dags/mtg-fetch.py
@@ -13,16 +13,26 @@ args = {
 }
 
 # Create the hive table
+# Filters out the unneeded data and maps json fields to sql fields via SerDe
 hiveSQL_create_table_mtg_cards='''
 CREATE EXTERNAL TABLE IF NOT EXISTS mtg_cards(
-	id STRING
-) COMMENT 'IMDb Ratings' PARTITIONED BY (partition_year int, partition_month int, partition_day int) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/title_ratings'
+	id STRING,
+	multiverseid INT,
+	name STRING,
+	imageUrl STRING,
+	artist STRING,
+	text STRING
+) COMMENT 'MTG Cards' PARTITIONED BY (partition_year int, partition_month int, partition_day int)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.contrib.serde2.JsonSerde'
+STORED AS TEXTFILE LOCATION '/user/hadoop/mtg'
 TBLPROPERTIES ('skip.header.line.count'='1');
 '''
 
 # Create the hive partition table
 hiveSQL_add_partition_mtg_cards='''
 ALTER TABLE mtg_cards
+ADD IF NOT EXISTS partition(partition_year={{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}, partition_month={{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}, partition_day={{ macros.ds_format(ds, "%Y-%m-%d", "%d")}})
+LOCATION '/user/hadoop/mtg/{{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%d")}}/';
 '''
 
 # Define DAG
@@ -34,28 +44,28 @@ dag = DAG('MTG Card Fetch', default_args=args, description='Fetches all up-to-da
 create_local_import_dir = CreateDirectoryOperator(
     task_id='create_import_dir',
     path='/home/airflow',
-    directory='mtg',
+    directory='mtg/raw',
     dag=dag,
 )
 clear_local_import_dir = ClearDirectoryOperator(
     task_id='clear_import_dir',
-    directory='/home/airflow/mtg',
+    directory='/home/airflow/mtg/raw',
     pattern='*',
     dag=dag,
 )
 
 # Fetch data from API
 download_title_ratings = HttpDownloadOperator(
-    task_id='download_title_ratings',
-    download_uri='https://datasets.imdbws.com/title.ratings.tsv.gz',
-    save_to='/home/airflow/imdb/title.ratings_{{ ds }}.tsv.gz',
+    task_id='download_mtg_cards',
+    download_uri='https://api.magicthegathering.io/v1/cards/',
+    save_to='/home/airflow/mtg/raw/raw_{{ ds }}.json',
     dag=dag,
 )
 
-# Create HDFS directory
+# Create HDFS directory separated by date: y/m/d
 create_hdfs_mtg_cards_partition_dir = HdfsMkdirFileOperator(
     task_id='mkdir_hdfs_mtg_cards_dir',
-    directory='/user/hadoop/mtg/cards/[PARTITION CRITERIA]',
+    directory='/user/hadoop/mtg/cards/{{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%d")}}',
     hdfs_conn_id='hdfs',
     dag=dag,
 )
@@ -63,16 +73,16 @@ create_hdfs_mtg_cards_partition_dir = HdfsMkdirFileOperator(
 # Put data into HDFS
 hdfs_put_mtg_cards = HdfsPutFileOperator(
     task_id='upload_mtg_cards_to_hdfs',
-    local_file='/home/airflow/imdb/[FILE]',
-    remote_file='/user/hadoop/imdb/title_ratings/[FILE]',
+    local_file='/home/airflow/mtg/raw/raw_{{ ds }}.json',
+    remote_file='/user/hadoop/mtg/{{ macros.ds_format(ds, "%Y-%m-%d", "%Y")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%m")}}/{{ macros.ds_format(ds, "%Y-%m-%d", "%d")}}/raw_{{ ds }}.json',
     hdfs_conn_id='hdfs',
     dag=dag,
 )
 
 # Create hive table
 create_HiveTable_mtg_cards = HiveOperator(
-    task_id='create_mtg_cards',
-    hql=hiveSQL_create_table_title_ratings,
+    task_id='create_mtg_cards_table',
+    hql=hiveSQL_create_table_mtg_cards,
     hive_cli_conn_id='beeline',
     dag=dag
 )
